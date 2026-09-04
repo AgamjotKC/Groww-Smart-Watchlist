40019935e8e5450a231d49eca38ff377288c2b54
Agamjot <agamjotkaurc001@gmail.com>
2026-09-04 16:58:26 +0530

    feat: add WatchlistStockRepository

---
+++ src/main/java/org/example/global/growwsmartwatchlist/repository/WatchlistStockRepository.java
@@ -0,0 +1,7 @@
+package org.example.global.growwsmartwatchlist.repository;
+
+import org.example.global.growwsmartwatchlist.model.WatchlistStock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WatchlistStockRepository extends JpaRepository<WatchlistStock, Long> {
+}
